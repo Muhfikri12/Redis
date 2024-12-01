077e2bf894990d623530405520ac8b7cd5950064
Fikri <fikrisacila0@gmail.com>
2024-12-01 16:27:14 +0700

    test: create testing for show redeem points

--- repository/management_voucher/management_voucher_test.go
+++ repository/management_voucher/management_voucher_test.go
@@ -167,4 +167,126 @@ func TestSoftDeleteVoucher(t *testing.T) {
 		assert.Error(t, err)
 		assert.EqualError(t, err, "database error")
 	})
+
+}
+
+func TestUpdateVoucher(t *testing.T) {
+	db, mock := setupTestDB()
+	defer func() { _ = mock.ExpectationsWereMet() }()
+
+	log := *zap.NewNop()
+	voucherRepo := managementvoucher.NewManagementVoucherRepo(db, &log)
+
+	t.Run("Successfully update a voucher", func(t *testing.T) {
+		voucherID := 1
+		voucher := &models.Voucher{
+			VoucherName:     "Promo Updated",
+			VoucherCode:     "UPDATED2024",
+			VoucherType:     "e-commerce",
+			PointsRequired:  10,
+			Description:     "Updated discount",
+			VoucherCategory: "discount",
+			DiscountValue:   15,
+			MinimumPurchase: 250000,
+			Quota:           50,
+		}
+
+		mock.ExpectBegin()
+		mock.ExpectExec(`UPDATE "vouchers" SET`).
+			WithArgs(
+				voucher.VoucherName,
+				voucher.VoucherCode,
+				voucher.VoucherType,
+				voucher.PointsRequired,
+				voucher.Description,
+				voucher.VoucherCategory,
+				voucher.DiscountValue,
+				voucher.MinimumPurchase,
+				voucher.Quota,
+				sqlmock.AnyArg(), // updated_at
+				voucherID,
+			).
+			WillReturnResult(sqlmock.NewResult(1, int64(voucherID)))
+		mock.ExpectCommit()
+
+		err := voucherRepo.UpdateVoucher(voucher, voucherID)
+		assert.NoError(t, err)
+	})
+
+	t.Run("Failed to update due to no matching record", func(t *testing.T) {
+		voucherID := 2
+		voucher := &models.Voucher{
+			VoucherName: "Promo Not Found",
+		}
+
+		mock.ExpectBegin()
+		mock.ExpectExec(`UPDATE "vouchers" SET`).
+			WithArgs(voucher.VoucherName, sqlmock.AnyArg(), voucherID).
+			WillReturnResult(sqlmock.NewResult(0, 0)) // No rows affected
+		mock.ExpectCommit()
+
+		err := voucherRepo.UpdateVoucher(voucher, voucherID)
+		assert.Error(t, err)
+		assert.EqualError(t, err, "no record found with shipping_id 2")
+	})
+
+	t.Run("Failed to update due to database error", func(t *testing.T) {
+		voucherID := 3
+		voucher := &models.Voucher{
+			VoucherName: "Promo Error",
+		}
+
+		mock.ExpectBegin()
+		mock.ExpectExec(`UPDATE "vouchers" SET`).
+			WithArgs(voucher.VoucherName, sqlmock.AnyArg(), voucherID).
+			WillReturnError(fmt.Errorf("database error"))
+		mock.ExpectRollback()
+
+		err := voucherRepo.UpdateVoucher(voucher, voucherID)
+		assert.Error(t, err)
+		assert.EqualError(t, err, "database error")
+	})
+}
+
+func TestShowRedeemPoints(t *testing.T) {
+	db, mock := setupTestDB()
+	defer func() { _ = mock.ExpectationsWereMet() }()
+
+	log := *zap.NewNop()
+	voucherRepo := managementvoucher.NewManagementVoucherRepo(db, &log)
+
+	t.Run("Successfully show redeem points", func(t *testing.T) {
+
+		mockRows := sqlmock.NewRows([]string{"voucher_name", "discount_value", "points_required"}).
+			AddRow("Promo A", 20.0, 50).
+			AddRow("Promo B", 15.0, 30)
+
+		mock.ExpectQuery(`SELECT v.voucher_name, v.discount_value, v.points_required FROM vouchers as v WHERE`).
+			WithArgs("redeem points").
+			WillReturnRows(mockRows)
+
+		result, err := voucherRepo.ShowRedeemPoints()
+		assert.NoError(t, err)
+		assert.NotNil(t, result)
+		assert.Len(t, *result, 2)
+
+		assert.Equal(t, "Promo A", (*result)[0].VoucherName)
+		assert.Equal(t, 20.0, (*result)[0].DiscountValue)
+		assert.Equal(t, 50, (*result)[0].PointsRequired)
+
+		assert.Equal(t, "Promo B", (*result)[1].VoucherName)
+		assert.Equal(t, 15.0, (*result)[1].DiscountValue)
+		assert.Equal(t, 30, (*result)[1].PointsRequired)
+	})
+
+	t.Run("Failed to show redeem points due to database error", func(t *testing.T) {
+		mock.ExpectQuery(`SELECT v.voucher_name, v.discount_value, v.points_required FROM vouchers as v WHERE`).
+			WithArgs("redeem points").
+			WillReturnError(fmt.Errorf("database error"))
+
+		result, err := voucherRepo.ShowRedeemPoints()
+		assert.Error(t, err)
+		assert.Nil(t, result)
+		assert.EqualError(t, err, "database error")
+	})
 }
